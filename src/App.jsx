5c4de5f69d1d3306b01200407dbf65ecac6308ce
Elvita Kalvāne <elvita.kalvane@gmail.com>
2021-11-27 11:14:10 +0100

    add data attribute to header

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,31 @@
-import React from 'react'
-import NewsSearch from "./components/NewsSearch"
-import { Container, Menu, Header, Image } from 'semantic-ui-react'
-import NewsIndex from './components/NewsIndex'
-import logo from './nwn.png'
-
+import React from "react";
+import NewsSearch from "./components/NewsSearch";
+import { Container, Menu, Header, Image } from "semantic-ui-react";
+import NewsIndex from "./components/NewsIndex";
+import logo from "./nwn.png";
 
 const App = () => {
   return (
     <>
-      <Menu style={{ borderRadius: 0, background: '' }}>
+      <Menu style={{ borderRadius: 0, background: "" }}>
         <Menu.Item>
-          <Image src={logo} size='tiny' centered />
+          <Image src={logo} size="tiny" centered />
         </Menu.Item>
-        <Menu.Menu position='right'>
+        <Menu.Menu position="right">
           <Menu.Item>
             <NewsSearch />
           </Menu.Item>
         </Menu.Menu>
       </Menu>
       <Container>
-
-        <Header as='h2' >
+        <Header as="h2" data-cy="news-header">
           News Wire Network
-          </Header>
+        </Header>
         <p>News from around the world</p>
         <NewsIndex />
       </Container>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
