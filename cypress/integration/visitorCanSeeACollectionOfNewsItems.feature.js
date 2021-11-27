5996e1864ee86be8ebb5c2619d091f79baa819ba
Elvita Kalvāne <elvita.kalvane@gmail.com>
2021-11-27 13:57:56 +0100

    WIP implementation of article dispaly need to find a mistake

--- cypress/integration/visitorCanSeeACollectionOfNewsItems.feature.js
+++ cypress/integration/visitorCanSeeACollectionOfNewsItems.feature.js
@@ -1,13 +1,18 @@
+/* eslint-disable no-undef */
 describe("Display latest news headlines", () => {
   beforeEach(() => {
-    cy.intercept("https://newsapi.org/v2/top-headlines/**", {
+    cy.intercept("GET", "https://newsapi.org/v2/top-headlines/**", {
       fixture: "news_index.json",
-    });
+    }).as("fetchData");
     cy.visit("/");
   });
 
   it("is expected to see a header", () => {
     cy.get("[data-cy=news-header]").should("contain", "News Wire Network");
+  });
+
+  it("is expected to return an array of data", () => {
+    cy.wait("@fetchData").its("response.body.data").should("be.an", "array");
   });
 
   it("is expected to see 38 list items", () => {
--- src/components/NewsIndex.jsx
+++ src/components/NewsIndex.jsx
@@ -1,14 +1,31 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import NewsCard from './NewsCard'
-
+import React, { useState, useEffect } from "react";
+//import { useSelector } from 'react-redux'
+import NewsCard from "./NewsCard";
+import { Grid } from "semantic-ui-react";
+import axios from "axios";
 
 const NewsIndex = () => {
+  const [topHeadlines, setTopHeadlines] = useState([]);
+
+  useEffect(() => {
+    getTopHeadlines();
+  }, []);
+
+  const getTopHeadlines = async () => {
+    const response = await axios.get(
+      `https://newsapi.org/v2/top-headlines?country=us&apiKey={process.env.REACT_APP_NEWS_API_KEY}`
+    );
+    setTopHeadlines(response.data.articles);
+  };
+
+  const listOfTopHeadlines = topHeadlines.map((article) => {
+    return <NewsCard article={article} />;
+  });
   return (
     <>
-      <NewsCard />
+      <Grid data-cy="articles">{listOfTopHeadlines}</Grid>
     </>
-  )
-}
+  );
+};
 
-export default NewsIndex
+export default NewsIndex;
