dcc0e6f67eda763eed0581a9bcd64941caa45d62
Elvita Kalvāne <elvita.kalvane@gmail.com>
2021-11-27 15:04:06 +0100

    add cards for dispalying and test content of the first one

--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -1,19 +1,15 @@
 import React from "react";
-import { Header, Card, Image } from "semantic-ui-react";
+import { Card, Image, Header } from "semantic-ui-react";
 
 const NewsCard = (news_index) => {
   return (
-    <Card>
-      <Image src={news_index.image} wrapped ui={false} />
-      <Card.Content>
-        <Card.Header>{news_index.title}</Card.Header>
-        <Card.Meta>
-          {`By: ${news_index.author} at ${news_index.source.name}`}{" "}
-        </Card.Meta>
-        <Card.Description>{news_index.description}</Card.Description>
-        <Card.Extra>{`Published at ${news_index.publishedAt}`}</Card.Extra>
-      </Card.Content>
-    </Card>
+    <Card
+      image="https://editorial.fxstreet.com/images/Markets/Currencies/Digital%20Currencies/Bitcoin/bitcoins-52602600_Large.jpg"
+      header="DBS Bank to launch cryptocurrency exchange after condemning Bitcoin a Ponzi scheme in 2017"
+      meta={`By: ${"Lorenzo Stroe"} at ${"FXStreet"}`}
+      description="DBS Bank of Singapore has just announced the launch of a digital asset exchange featuring Bitcoin, Ethereum, Bitcoin Cash, and XRP and several fiat cu"
+      extra={<Header as="h6">`Published at ${"2020-12-10T20:29:20Z"}`</Header>}
+    />
   );
 };
 
